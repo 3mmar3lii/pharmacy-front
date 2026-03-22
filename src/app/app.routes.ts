bca1946401f03c96a664a15f4355efe175ea6e37
Hala129 <halaehab129@gmail.com>
2026-03-22 12:22:59 +0200

    product , product-detais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Auth } from './core/layout/auth-layout/auth';
+import { User } from './core/layout/user-layout/user';
+import { Login } from './core/auth/component/login/login';
+import { Register } from './core/auth/component/register/register';
+import { Home } from './feature/home/component/home/home';
+import { ProductList } from './feature/product/component/product-list/product-list';
+import { NotFound } from './core/auth/component/not-found/not-found';
+import { ProductDetails } from './feature/product/component/product-details/product-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: '', component: Auth, children: [
+      // { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+      // {path: '**', component:NotFound },
+    ],
+  },
+  {
+    path: '', component: User, children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: Home },
+      { path: 'product', component: ProductList },
+      { path: 'product-details/:id', component: ProductDetails },
+      {path: '**', component:NotFound },
+    ],
+  },
+
+];
