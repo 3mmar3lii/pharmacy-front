00d10ae606d89fbd240c4ea18ec23cb4bf457806
3mmar 3li <ammaralaa572@gmail.com>
2026-03-23 12:30:58 +0200

    fix first page login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
     path: '', 
     loadComponent: () => import('./auth/auth-layout/auth-layout.component').then(m => m.AuthLayoutComponent),
     children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
       { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent) },
     ],
