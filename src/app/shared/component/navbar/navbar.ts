4a96c44e6158536e82a25489187db12c787b6195
Hala129 <halaehab129@gmail.com>
2026-03-26 22:55:12 +0200

    modify the navbar interface and add filteration

--- src/app/shared/component/navbar/navbar.ts
+++ src/app/shared/component/navbar/navbar.ts
@@ -1,17 +1,57 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ProductsService } from '../../../feature/product/services/products';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
-@Input() layout!: string;
-currentLang: 'en' | 'ar' = 'ar';
+export class Navbar implements OnInit {
 
-  toggleLang() {
-    this.currentLang = this.currentLang === 'en' ? 'ar' : 'en';
+  allItems: any[] = [];
+  filteredItems: any[] = [];
+
+  private _filteration: string = '';
+
+  currentLanguage: string = 'EN';
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.productsService.getProducts().subscribe((res) => {
+      this.allItems = res.data;
+      this.filteredItems = [];
+    });
+  }
+
+  get filteration(): string {
+    return this._filteration;
+  }
+
+  set filteration(value: string) {
+    this._filteration = value;
+    this.filteredItems = this.filterItems(value);
+  }
+
+  filterItems(search: string): any[] {
+    const value = search?.toLowerCase() || '';
+    if (!value) return [];
+    return this.allItems.filter(item => item.title.toLowerCase().startsWith(value));
+  }
+  closeDropdown() {
+    this.filteredItems = [];
+    this.filteration = '';
+  }
+  switchLanguage(language: string) {
+    this.currentLanguage = language;
+    console.log('Switched to', language);
   }
 }
